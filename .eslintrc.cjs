aecbedbc59428e0a9317cbedf8485711aaef2023
Greg Rickaby <gregrickaby@gmail.com>
2024-02-02 09:44:49 -0600

    fix eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,7 +1,7 @@
 /* eslint-env node */
 require('@rushstack/eslint-patch/modern-module-resolution')
 
-export default {
+module.exports = {
   root: true,
   extends: [
     'plugin:vue/vue3-essential',
