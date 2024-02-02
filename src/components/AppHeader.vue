14380f9d29ae179bdbbb111aaab87a4fc9c5b86b
Greg Rickaby <gregrickaby@gmail.com>
2024-02-02 15:42:54 -0600

    add logo

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,7 +1,10 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import logo from '@/assets/logo.png'
+</script>
 
 <template>
-  <header class="flex flex-col items-center justify-center gap-4">
-    <h1>Spelling Scramble</h1>
+  <header class="flex flex-col items-center justify-center">
+    <h1 class="sr-only">Spelling Scramble</h1>
+    <img :src="logo" alt="Spelling Scramble" class="m-0 w-[400px] pb-8" loading="eager" />
   </header>
 </template>
