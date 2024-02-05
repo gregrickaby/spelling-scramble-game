a370a912e67492ae4c7daf9094711c955917def1
Greg Rickaby <gregrickaby@gmail.com>
2024-02-05 07:55:55 -0600

    add presisted state

--- src/stores/spellingStore.ts
+++ src/stores/spellingStore.ts
@@ -1,20 +1,28 @@
+import playSound from '@/utils/sounds'
 import { defineStore } from 'pinia'
 
-export const useSpellingStore = defineStore('spelling', {
+/**
+ * This store is used to manage the state of the game.
+ *
+ * @see https://pinia.vuejs.org/
+ * @see https://prazdevs.github.io/pinia-plugin-persistedstate/
+ */
+export const useSpellingStore = defineStore('spellingScramble', {
   state: () => ({
-    words: [] as string[],
     currentIndex: 0,
+    enablePersistance: true,
     gameCompleted: false,
+    gameStarted: false,
     message: '',
-    points: 0,
-    name: ''
+    userName: '',
+    userPoints: 0,
+    words: [] as string[]
   }),
   getters: {
     currentWord(state): string {
-      return state.words[state.currentIndex] || '' // Return an empty string if undefined
+      return state.words[state.currentIndex] || ''
     },
     scrambledWord(): string {
-      // Ensure there's a word to scramble
       if (this.currentWord) {
         const shuffled = this.currentWord
           .split('')
@@ -22,28 +30,29 @@ export const useSpellingStore = defineStore('spelling', {
           .join('')
         return shuffled
       }
-      return '' // Return an empty string if currentWord is undefined or empty
+      return ''
     }
   },
   actions: {
-    addWords(newWords: string[]) {
-      this.words.push(...newWords)
-    },
-    addName(newName: string) {
-      this.name = newName
+    initializeGame(words: string[], name: string) {
+      this.userName = name
+      this.words = words
+      this.gameStarted = true
+      playSound('start')
     },
     nextWord() {
       if (this.currentIndex < this.words.length - 1) {
         this.currentIndex++
-        this.gameCompleted = false // Reset completion state if not the last word
+        this.gameCompleted = false
         this.setMessage('🥳 Correct!')
         this.addPoints(this.currentWord.length)
+        playSound('correct')
       } else {
-        this.gameCompleted = true // Set to true to indicate game completion
+        this.gameCompleted = true
         this.addPoints(50)
       }
     },
-    setMessage(newMessage) {
+    setMessage(newMessage: string) {
       this.message = newMessage
       setTimeout(() => this.clearMessage(), 2000)
     },
@@ -51,14 +60,15 @@ export const useSpellingStore = defineStore('spelling', {
       this.message = ''
     },
     addPoints(points: number) {
-      this.points += points
+      this.userPoints += points
     },
     resetGame() {
-      this.words = []
       this.currentIndex = 0
-      this.gameCompleted = false // Reset completion state
-      this.message = '' // Clear message
-      this.points = 0 // Reset points
+      this.gameCompleted = false
+      this.gameStarted = false
+      this.message = ''
+      this.userPoints = 0
     }
-  }
+  },
+  persist: true
 })
