28ec25cbbc23f253464384892c4740205bf0e856
Greg Rickaby <gregrickaby@gmail.com>
2024-02-05 07:56:28 -0600

    add NameAndPoints component

---
+++ src/components/NameAndPoints.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { useCounter } from '@/composables/useCounter'
+import { useSpellingStore } from '@/stores/spellingStore'
+import { watch } from 'vue'
+
+const store = useSpellingStore()
+
+const { displayValue, animateTo } = useCounter()
+
+watch(
+  () => store.userPoints,
+  (newPoints) => {
+    animateTo(newPoints)
+  },
+  { immediate: true }
+)
+</script>
+
+<template>
+  <div v-if="store.gameStarted" class="points-display">
+    <span v-if="store.userName">{{ store.userName }}'s</span> Points: {{ displayValue }}
+  </div>
+</template>
