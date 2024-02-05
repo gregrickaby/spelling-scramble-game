9340e09892b540047ae3bcc99c7b31a51df29525
Greg Rickaby <gregrickaby@gmail.com>
2024-02-05 07:56:19 -0600

    register persisted state

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,16 @@
 import '@/assets/globals.css'
 
 import { createPinia } from 'pinia'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 import { createApp } from 'vue'
 
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
-
-app.use(createPinia())
+const pinia = createPinia()
+pinia.use(piniaPluginPersistedstate)
+app.use(pinia)
 app.use(router)
 
 app.mount('#app')
