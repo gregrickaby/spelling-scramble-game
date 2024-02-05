86748baa4cb3a3e4b1f7bf6e584bd7b6eec8b626
Greg Rickaby <gregrickaby@gmail.com>
2024-02-05 10:13:05 -0600

    move messages to store

--- src/components/DisplayMessage.vue
+++ src/components/DisplayMessage.vue
@@ -5,22 +5,11 @@ const store = useSpellingStore()
 </script>
 
 <template>
-  <div v-if="store.gameCompleted" class="message">
-    🎉 Congratulations <span v-if="store.userName">{{ store.userName }}</span
-    >! You've spelled all the words correctly!
-    <a href="" @click="store.resetGame">Play Again?</a> 🐝
-  </div>
-
   <div v-if="store.message" class="message">{{ store.message }}</div>
 </template>
 
 <style scoped>
 .message {
-  margin-top: 20px;
-  color: #31708f;
-  background-color: #d9edf7;
-  border-color: #bce8f1;
-  padding: 10px;
-  border-radius: 4px;
+  @apply mt-2 rounded border border-blue-300 bg-blue-100 p-2;
 }
 </style>
